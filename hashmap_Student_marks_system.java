bf8377bf883ed9895cbcfe7de3819bc52246695b
rite2harini <rite2harini@gmail.com>
2026-06-10 15:11:26 +0530

    Add HashMap for student marks management
    Implemented a HashMap to manage student records, allowing for adding, retrieving, and checking student IDs.

--- hashmap_Student_marks_system.java
+++ hashmap_Student_marks_system.java
@@ -17,4 +17,26 @@ Hint - use HashMap
 put()
 get()
 containsKey()
+
+Instructions - 
+Create a HashMap called students
+
+Add student records:
+    101 → John
+    102 → Priya
+    103 → Rahul
+    104 → David
+    105 → Anu
+
+Display all student records
+
+Ask the user to enter a Student ID
+
+Read the Student ID
+
+IF the Student ID exists in the HashMap THEN
+       Get the student name using the ID
+       Display the student name
+ELSE  Display "Student Not Found"
+END IF
   */
